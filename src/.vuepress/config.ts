bf999cfc38da6b60d65ab788e67e93c3e743eac3
TianYi2023 <1299740555@qq.com>
2025-03-26 12:19:52 +0800

    更新VuePress配置和部署工作流

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,15 +1,25 @@
 import { defineUserConfig } from "vuepress";
-
+import { path } from "@vuepress/utils";
 import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/",
+  dest: "E:/rang ke yan xiang/my-docs/.vuepress/dist",
 
   lang: "zh-CN",
   title: "文档演示",
   description: "vuepress-theme-hope 的文档演示",
 
   theme,
+
+  // 配置主页
+  locales: {
+    "/": {
+      lang: "zh-CN",
+      title: "项目主页",
+      description: "项目描述",
+    },
+  },
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
